5f015da2b8ac72ed328a749eb99737183f80a343
Mahesh Bahir <maheshbahir45@gmail.com>
2024-04-21 11:58:00 +0530

    Add feature/createFileTask, showFileTask

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 // Handle data from Backend
 app.use(express.json());
@@ -13,16 +14,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render("index")
+  fs.readdir(`./files`, function(err, files){
+    //console.log(files);
+    res.render("index", {files: files});
+  })
 });
 
-app.get('/profile/:username', (req, res) => {
-    res.send(`Welcome, ${req.params.username}`);
+app.get('/file/:filename', function(req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+    //console.log(filedata);
+    res.render('show', {filename: req.params.filename, filedata: filedata});
+  })
 });
 
-app.get('/author/:username/:age', (req, res) => {
-  res.send(`Welcome, ${req.params.username} of age ${req.params.age}`);
+app.post('/create', function(req, res) {
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+    res.redirect("/");
+  });
 });
+
+//Dynamic routing
+// app.get('/profile/:username', (req, res) => {
+//     res.send(`Welcome, ${req.params.username}`);
+// });
+
+// app.get('/author/:username/:age', (req, res) => {
+//   res.send(`Welcome, ${req.params.username} of age ${req.params.age}`);
+// });
 
 app.listen(4000, function() {
   console.log("its running")
